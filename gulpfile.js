0c81215eb371b3b1a0858ccc9a8c42afb36331eb
cr15p1 <cspeck89+ghub@gmail.com>
2022-12-09 17:08:51 +0100

    deploy github page

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+/* eslint-disable global-require */
+/* eslint-disable no-param-reassign */
 /* eslint-disable @typescript-eslint/unbound-method */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
@@ -5,10 +7,58 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { series, src, dest } = require("gulp");
+const { series, src, dest, t } = require("gulp");
 const { readFileSync } = require("fs");
 const { EOL } = require("os");
 const replace = require("gulp-replace");
+const tap = require("gulp-tap");
+
+const markdownIt = require("markdown-it");
+
+// require markdown-it plugins
+const abbr = require("markdown-it-abbr");
+const alerts = require("markdown-it-alerts");
+const anc = require("markdown-it-anchor");
+const attrs = require("markdown-it-attrs");
+const figs = require("markdown-it-implicit-figures");
+const kbd = require("markdown-it-kbd");
+const toc = require("markdown-it-table-of-contents");
+const list = require("markdown-it-task-lists");
+const highlight = require("markdown-it-highlightjs");
+
+// Markdown-It Options
+const options = {
+  preset: "default",
+  html: true,
+  xhtmlOut: true,
+  linkify: true,
+  typographer: true,
+};
+
+const md = markdownIt("commonmark", options)
+  .use(highlight, {
+    auto: true,
+
+    register: {
+      typescript: require("highlight.js/lib/languages/typescript"),
+    },
+    hljs: require("highlight.js"),
+  })
+  .use(abbr)
+  .use(alerts)
+  .use(anc)
+  .use(attrs)
+  .use(figs)
+  .use(kbd)
+  .use(toc)
+  .use(list);
+
+function markdownToHtml(file) {
+  const result = md.render(file.contents.toString());
+  console.log(result);
+  file.contents = Buffer.from(result);
+  file.path = file.path.replace(/\.md$/, ".html");
+}
 
 const parser = {
   code: (config, fileBase) => {
@@ -22,7 +72,7 @@ const parser = {
     return [`\`\`\`${config.language || ""}`, fileContent, "```"].join(EOL);
   },
 };
-const buildDocks = () =>
+const markdown = () =>
   src("docsSrc/**/*.md", { buffer: true })
     .pipe(
       replace(/<!---\{(.*)\}--->/g, function (_, p1) {
@@ -31,6 +81,20 @@ const buildDocks = () =>
           .join("");
       })
     )
-    .pipe(dest("docs"));
+    .pipe(tap(markdownToHtml))
+    .pipe(
+      tap((file) => {
+        file.contents = Buffer.from(
+          [
+            `<link rel="stylesheet" type="text/css" href="vs2015.css">`,
+            file.contents.toString(),
+          ].join(" ")
+        );
+      })
+    )
+    .pipe(dest("_site"));
 
-module.exports.default = series(buildDocks);
+const styles = () =>
+  src("node_modules/highlight.js/styles/vs2015.css").pipe(dest("_site"));
+
+module.exports.default = series(markdown, styles);
